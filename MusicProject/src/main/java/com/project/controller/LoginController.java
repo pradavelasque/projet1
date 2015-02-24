64caa25fad6ae2073ff9e7ae3671d225c031d0e8
pradavelasque <pradavelasque@gmail.com>
2015-02-23 22:19:16 -0500

    ajout js pour youtube

---
+++ MusicProject/src/main/java/com/project/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.project.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	private final String IDENTIFIANT = "identifiant"; 
+	
+//	@RequestMapping(value="", method=RequestMethod.GET)
+//	public String pageGlobal(Locale locale, Model model){
+//		
+//		return IDENTIFIANT;
+//		
+//	}
+
+}
---
+++ MusicProject/src/main/java/com/project/music/Music.java
@@ -0,0 +1,20 @@
+package com.project.music;
+
+public class Music {
+
+	private String name;
+	private String adresse;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+}
