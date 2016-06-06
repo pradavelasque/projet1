b594a4490d6a8ec5a39f0e1389c7b2d10fe50ce5
pradavelasque@gmail.com <pradavelasque@gmail.com>
2016-06-06 19:10:54 -0400

    mise a jour

--- findMe/src/main/java/com/findMe/controller/ControlleurPosition.java
+++ findMe/src/main/java/com/findMe/controller/ControlleurPosition.java
@@ -1,32 +1,38 @@
 package com.findMe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControlleurPosition {
 	
 	private  static String ACCEUILLE_PAGE = "/";
 	
+	@RequestMapping("/initialiser")
 	public String initialiserPosition(){
 	
 		return ACCEUILLE_PAGE;
 	}
 	
+	@RequestMapping("/sendPosition")
 	public String sendPosition(){
 		
 		return ACCEUILLE_PAGE;
 	}
 	
+	@RequestMapping("/synchroniserPosition")
 	public String synchroniserPosition(){
 		
 		return ACCEUILLE_PAGE;
 	}
 	
+	@RequestMapping("/demandePosition")
 	public String demandePosition(){
 		
 		return ACCEUILLE_PAGE;
 	}
 	
+	@RequestMapping("/arreterSynchronisation")
 	public String arreterSynchronisation(){
 		
 		return ACCEUILLE_PAGE;
